0712b8a0aa73d9f05392681fa863fbff7dfd804c
marcelboo <101633688+marcelboo@users.noreply.github.com>
2026-04-13 15:39:38 +0200

    update the soundmanager to include music

--- Engine/SoundManager.cs
+++ Engine/SoundManager.cs
@@ -8,7 +8,7 @@ namespace SpellFall.Engine
     public class SoundManager
     {
         public Dictionary<string, SoundEffect> Sounds = new Dictionary<string, SoundEffect>();
-
+        private SoundEffectInstance currentMusic;
         public void Load(ContentManager content, string name, string assetsPath)
         {
             if (!Sounds.ContainsKey(name))
@@ -21,14 +21,44 @@ namespace SpellFall.Engine
                 Console.WriteLine($"Sound '{name}' already loaded!");
             }
         }
-
-        
         public void PlaySound(string name, float volume, float pitch = 0f, float pan = 0f)
         {
             if(Sounds.TryGetValue(name,out SoundEffect soundEffect))
             {
                 soundEffect.Play(volume, pitch, pan);
             }
+        }
+        public void PlayMusic(string name, bool loop, float volume)
+        {
+            if (Sounds.TryGetValue(name, out SoundEffect soundEffect))
+            {
+                currentMusic?.Stop();
+
+                currentMusic = soundEffect.CreateInstance();
+                currentMusic.IsLooped = loop;
+                currentMusic.Volume = 0.5f;
+                currentMusic.Play();
+            }   
+            else
+            {
+                Console.WriteLine($"Music '{name}' not found!");
+            }
         }   
+        public void PauseMusic()
+        {
+            currentMusic.Pause();
+        }
+        public void ResumeMusic()
+        {
+            currentMusic.Resume();
+        }
+        public void StopMusic()
+        {
+            currentMusic.Stop();
+        }
+        public void SetMusicVolume(float volume)
+        {
+            currentMusic.Volume = volume;
+        }
     }
 }
