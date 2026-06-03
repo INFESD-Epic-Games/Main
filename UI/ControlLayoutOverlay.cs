20cc0e224adaccb1061556c177d123d7e92aba1c
marcelboo <1038695@hr.nl>
2026-06-03 12:34:38 +0200

    added the tutorial for inventory

--- UI/ControlLayoutOverlay.cs
+++ UI/ControlLayoutOverlay.cs
@@ -1,3 +1,4 @@
+using System.Reflection.Metadata.Ecma335;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
@@ -10,10 +11,15 @@ namespace SpellFall.UI
         private static readonly Vector2 WorldPosition = new Vector2(320f, 640f);
 
         private Texture2D _texture;
+        private Texture2D _textureInventory;
+        private Vector2 position = new Vector2(2600, 600); 
+
+        private float scale = 8;
 
         public override void Load(ContentManager content)
         {
             _texture = content.Load<Texture2D>("Control_layout");
+            _textureInventory = content.Load<Texture2D>("inventory tut");
             base.Load(content);
         }
 
@@ -23,9 +29,18 @@ namespace SpellFall.UI
             {
                 return;
             }
-
             spriteBatch.Draw(_texture, WorldPosition, Color.White);
-
+            spriteBatch.Draw(
+                _textureInventory,
+                position,
+                null,
+                Color.White,
+                0f,
+                Vector2.Zero,
+                scale,
+                SpriteEffects.None,
+                0f
+            );
             base.Draw(gameTime, spriteBatch);
         }
     }
